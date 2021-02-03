12d8049a71d55fe8bfc44c59fb3d361dff44f7e9
Zach Shearer <shearertzach@gmail.com>
2021-02-02 20:38:10 -0600

    Added the first 4 methods to the song class

--- Song.py
+++ Song.py
@@ -1,28 +1,26 @@
 class Song:
 
-  def __init__(self, title):
+    def __init__(self, title):
       self.__title = title
       self.__next_song = None
 
+    def __init__(self, title):  
+        self.__title = title
+        self.__next_song = None
 
-  # TODO: Create a getter method for the title attribute, called get_title
-  def get_title(self):
-    pass
-  
-  
-  # TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
-  def set_title(self, title):
-    pass
+    def get_title(self):
+        return self.__title
 
+    def set_title(self, title):
+        old_title = self.__title
+        self.__title = title
+        return f"You have renamed {old_title} to {title}"
 
-  # TODO: Create a getter method for the next_song attribute, called get_next_song
-  def get_next_song(self):
-    pass
-
-
-  # TODO: Create a setter method for the next_song attribute, called set_next_song
-  def set_next_song(self, next_title):
-    pass
+    def get_next_song(self):
+        if self.__next_song != None:
+            return self.__next_song
+        else:
+            return None
 
 
   # TODO: Using the __str___ dunder method, return a string of the song title.
