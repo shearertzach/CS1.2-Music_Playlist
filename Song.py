dbe3f09476d3531ae14fba45f173a69748e825bd
Zach Shearer <shearertzach@gmail.com>
2021-02-02 20:39:24 -0600

    Finished song class and removed errors

--- Song.py
+++ Song.py
@@ -4,10 +4,6 @@ class Song:
       self.__title = title
       self.__next_song = None
 
-    def __init__(self, title):  
-        self.__title = title
-        self.__next_song = None
-
     def get_title(self):
         return self.__title
 
@@ -21,13 +17,13 @@ class Song:
             return self.__next_song
         else:
             return None
+            
+    def set_next_song(self, next_title):
+        self.__next_song = next_title
 
 
-  # TODO: Using the __str___ dunder method, return a string of the song title.
-  def __str__(self):
-    pass
+    def __str__(self):
+        return self.__title
 
-
-  # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
-  def __repr__(self):
-    pass
+    def __repr__(self):
+        return f"{self.__title} -> {self.__next_song}"
