f9610727cfee8454e42cb7aff8955e3685ef4032
Zach Shearer <shearertzach@gmail.com>
2021-02-02 20:36:05 -0600

    Created first 3 functions to the playlist class

--- Playlist.py
+++ Playlist.py
@@ -5,25 +5,51 @@ class Playlist:
     self.__first_song = None
 
 
-  # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
+def add_song(self, title):
+        current = self.__first_song
+        new_song = Song(title)
 
-  def add_song(self, title):
-    pass
+        if current == None:
+            self.__first_song = new_song
+        else:
+            while current.get_next_song() != None:
+                current = current.get_next_song()
+            current.set_next_song(new_song)
 
+    def find_song(self, title):
+        current = self.__first_song
+        found = False
+        index = 0
 
+        while current != None and not found:
+            if current.get_title() == title:
+                found = True
+            else:
+                current = current.get_next_song()
+                index + 1
 
-  # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
+        return index
 
-  def find_song(self, title):
-    pass
+    def remove_song(self, title):
+        current = self.__first_song
+        prev = None
+        found = False
 
+        while not found:
+            if current == None:
+                print(f"Could not find song with name {title}")
+                found = True
+                return
+            elif current.get_title() == title:
+                found = True
+            else:
+                prev = current
+                current = current.get_next_song()
 
-  # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
-
-  def remove_song(self, title):
-    pass
-
-
+        if prev == None:
+            self.__first_song = current.get_next_song()
+        else:
+            prev.set_next_song(current.get_next_song())
 
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
